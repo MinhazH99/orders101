a17a3be88b422faa8d145c4dee5fffdb8884899e
Minhaz Hassan <minhaz.hassan@valtech.com>
2023-10-16 17:08:08 +0100

    Test commit

--- src/main/java/com/minhaz/orders101/Orders101Application.java
+++ src/main/java/com/minhaz/orders101/Orders101Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Orders101Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(Orders101Application.class, args);
 	}
-
 }
