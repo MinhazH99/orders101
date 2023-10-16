f4c9ed0852aa4b036cbf9e1bf9220d19b7d29264
Minhaz Hassan <minhaz.hassan@valtech.com>
2023-10-16 17:04:08 +0100

    Initial commit

---
+++ src/main/java/com/minhaz/orders101/Orders101Application.java
@@ -0,0 +1,13 @@
+package com.minhaz.orders101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Orders101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Orders101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhaz/orders101/Orders101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaz.orders101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Orders101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
