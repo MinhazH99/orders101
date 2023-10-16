a58476c360d6bdf4501439b0d61c87a77a7d4104
Minhaz Hassan <minhaz.hassan@valtech.com>
2023-10-16 17:45:08 +0100

    Test commit

--- src/main/java/com/minhaz/orders101/Orders101Application.java
+++ src/main/java/com/minhaz/orders101/Orders101Application.java
@@ -8,7 +8,6 @@ public class Orders101Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Test");
 		SpringApplication.run(Orders101Application.class, args);
 	}
 }
