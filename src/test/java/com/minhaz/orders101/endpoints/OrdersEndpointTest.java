e92ce4c0a85af7050a72143eedd429283cb8f7e0
MinhazH99 <minhazh@hotmail.co.uk>
2023-11-03 14:58:19 +0000

    Removed endpoint test

--- src/test/java/com/minhaz/orders101/endpoints/OrdersEndpointTest.java
+++ src/test/java/com/minhaz/orders101/endpoints/OrdersEndpointTest.java
@@ -1,21 +1,16 @@
 package com.minhaz.orders101.endpoints;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.net.http.HttpResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class OrdersEndpointTest {
+
 //    @Test
-////
-////    public void getOrdersTest() {
-//////        OrdersEndpoint ordersEndpoint = new OrdersEndpoint();
-//////        assertEquals("success",ordersEndpoint.getOrders());
-////    }
+//    public void getOrdersTest() {
+//        OrdersEndpoint ordersEndpoint = new OrdersEndpoint();
+//        assertEquals("success",ordersEndpoint.getOrders());
+//    }
 
     @Test
     public void testGetRequest() {
