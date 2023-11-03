06074fe58536dde4e269a52ff9647195cb103d93
MinhazH99 <minhazh@hotmail.co.uk>
2023-11-03 14:50:34 +0000

    Removing endpoint test and updating pre-commit

--- src/test/java/com/minhaz/orders101/endpoints/OrdersEndpointTest.java
+++ src/test/java/com/minhaz/orders101/endpoints/OrdersEndpointTest.java
@@ -10,15 +10,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class OrdersEndpointTest {
-    @Test
-
-    public void getOrdersTest() {
-        OrdersEndpoint ordersEndpoint = new OrdersEndpoint();
-        assertEquals("success",ordersEndpoint.getOrders());
-    }
+//    @Test
+////
+////    public void getOrdersTest() {
+//////        OrdersEndpoint ordersEndpoint = new OrdersEndpoint();
+//////        assertEquals("success",ordersEndpoint.getOrders());
+////    }
 
     @Test
     public void testGetRequest() {
+
 
     }
 
