6022275aa666320f314154edc94f05aaa4bb9443
MinhazH99 <minhazh@hotmail.co.uk>
2023-11-09 15:23:46 +0000

    Updated data inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,28 @@
+INSERT INTO ADDRESS
+VALUES
+    ('1','Test Street','T3ST','England');
+
 INSERT INTO CUSTOMER
 VALUES
-    ('1','cust1@gmail.com','John');
+    ('1','cust1@gmail.com','1','John');
 
 INSERT INTO LINE_ITEM
 VALUES
-    ('15',125.10,'test','hammer','1'),
-    ('25',100,'test1','screwdriver','2'),
-    ('40',65,'test2','drill','3');
+    ('15',125.10,'test','1','hammer'),
+    ('25',100,'test1','2','screwdriver'),
+    ('40',65,'test2','3','drill');
 
 INSERT INTO BASKET
 VALUES
     ('1');
 
-INSERT INTO BASKET_PRODUCT
+INSERT INTO BASKET_LINE_ITEMS
 VALUES
     ('1','1'),
     ('1','2'),
     ('1','3');
 
+
 INSERT INTO orders
 VALUES
-    (DATE '2023-10-30', 0,0,'1','1','1');
+    (DATE '2023-10-30','1','1','1','1','COMPLETED','AUTHORISED');
