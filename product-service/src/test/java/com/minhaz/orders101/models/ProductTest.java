8c6375822b3727dc0042034532837666d78de4e1
MinhazH99 <minhazh@hotmail.co.uk>
2023-12-11 11:27:29 +0000

    #20 - Created ProductUtils and test for Getters

--- product-service/src/test/java/com/minhaz/orders101/models/ProductTest.java
+++ product-service/src/test/java/com/minhaz/orders101/models/ProductTest.java
@@ -1,16 +1,35 @@
 package com.minhaz.orders101.models;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static com.minhaz.orders101.utils.ProductUtils.sampleProduct;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class ProductTest {
 
+  private Product productUnderTest;
+
+  @BeforeEach
+  void instantiateProduct() {
+    productUnderTest = sampleProduct().build();
+  }
+
 
 
   @Test
-  @Disabled
   void testGetters() {
+    assertAll("Product details", () -> assertEquals("1", productUnderTest.getId()),
+        () -> assertEquals(10, productUnderTest.getStockLevel()),
+        () -> assertEquals(new BigDecimal("15.50"), productUnderTest.getUnitPrice()),
+        () -> assertEquals("tool", productUnderTest.getDescription()),
+        () -> assertEquals("screwdriver", productUnderTest.getName()));
 
   }
 
