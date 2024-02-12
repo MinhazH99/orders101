3df814410535edc002b0a740a7ed20c36cb88634
MinhazH99 <minhazh@hotmail.co.uk>
2024-02-12 16:00:52 +0000

    Editing script to retreive trending products

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,6 @@ function hideSideBar() {
 
 // Specify the API endpoint for user data
 const apiUrl = 'http://localhost:8080/products/';
-
 // Make a GET request using the Fetch API
 fetch(apiUrl)
     .then((response) => {
@@ -20,44 +19,37 @@ fetch(apiUrl)
         return response.json();
     })
     .then((data) => {
-        // Process the retrieved user data
         data.data.forEach((element) => {
-            console.log(element);
-            var anchor = document.createElement('a');
-            var divWithProductName = document.createElement('div');
-            productName = document.createTextNode(element.name);
-            divWithProductName.appendChild(productName);
-            var divWithUnitPrice = document.createElement('div');
-            unitPrice = document.createTextNode(element.unitPrice);
-            divWithUnitPrice.append(unitPrice);
-            anchor.appendChild(divWithProductName);
-            anchor.appendChild(divWithUnitPrice);
+            if ('content' in document.createElement('template')) {
+                const trendingBody = document.querySelector('#trending');
+                const template = document.querySelector('#trending-product');
 
-            const trendingProduct = document.getElementById('trending');
-            trendingProduct.appendChild(anchor);
+                const clone = template.content.cloneNode(true);
+                let tp = clone.querySelectorAll('div');
+                tp[1].textContent = element.name;
+                tp[2].textContent = element.unitPrice;
+
+                trendingBody.appendChild(clone);
+            } else {
+                tp[0].textContent = 'Placeholder';
+                tp[1].textContent = '£XX.XX';
+            }
         });
     })
     .catch((error) => {
         console.error('Error:', error);
 
         for (let i = 0; i < 4; i++) {
-            var anchor = document.createElement('a');
-            var divWithImage = document.createElement('div');
-            var productImage = document.createElement('img');
-            productImage.src = './assets/images/trending-product.webp';
-            productImage.className = 'site-image';
-            divWithImage.appendChild(productImage);
-            var divWithProductName = document.createElement('div');
-            var productName = document.createTextNode('Placeholder');
-            divWithProductName.appendChild(productName);
-            anchor.appendChild(divWithProductName);
-            var divWithUnitPrice = document.createElement('div');
-            var unitPrice = document.createTextNode('£XX.XX');
-            divWithUnitPrice.append(unitPrice);
-            anchor.appendChild(divWithImage);
-            anchor.appendChild(divWithProductName);
-            anchor.appendChild(divWithUnitPrice);
-            const trendingProduct = document.getElementById('trending');
-            trendingProduct.appendChild(anchor);
+            if ('content' in document.createElement('template')) {
+                const trendingBody = document.querySelector('#trending');
+                const template = document.querySelector('#trending-product');
+                const clone = template.content.cloneNode(true);
+                let tp = clone.querySelectorAll('div');
+                tp[0].querySelector('img').src = './assets/images/trending-product.webp';
+                tp[1].textContent = 'Placeholder';
+                tp[2].textContent = '£XX.XX';
+
+                trendingBody.appendChild(clone);
+            }
         }
     });
