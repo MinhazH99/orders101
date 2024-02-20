25d6854421e6460b44fe91a6bb2f4b9ea208f3d3
MinhazH99 <minhazh@hotmail.co.uk>
2024-02-20 16:00:04 +0000

    check for empty elements

--- frontend/javascript/products.js
+++ frontend/javascript/products.js
@@ -21,6 +21,7 @@ window.onload = (event) => {
 
 function createProductImage(template) {
     let prodImage = template.querySelector('#trending-div-product-image');
+    checkIfDivExists(prodImage);
     // Need to test for null image once image is added to API
     prodImage.setAttribute('src', './assets/images/trending-product.webp');
     prodImage.setAttribute('alt', 'Image of a trending product');
@@ -28,6 +29,7 @@ function createProductImage(template) {
 
 function createProductLabel(template, product) {
     let labelDiv = template.querySelector('#trending-div-product-label');
+    checkIfDivExists(labelDiv);
     if (typeof product.name === 'undefined' || product.name === null) {
         labelDiv.textContent = 'Error loading name...';
     } else {
@@ -35,6 +37,7 @@ function createProductLabel(template, product) {
     }
 
     let priceDiv = template.querySelector('#trending-div-product-price');
+    checkIfDivExists(priceDiv);
     if (typeof product.unitPrice === 'undefined' || product.unitPrice === null) {
         priceDiv.textContent = 'Error loading price...';
     } else {
@@ -49,13 +52,18 @@ function formatPrice(product) {
     });
 }
 
+function checkIfDivExists(div) {
+    if (typeof div == 'undefined' || div === null) {
+        return console.error('Error: Div does not exist');
+    }
+}
+
 const supportsTemplate = () => 'content' in document.createElement('template');
 
 let doesBrowserSupportTemplete = supportsTemplate();
 
 // Work in progress
 function createTemplate() {
-    const trendingDiv = document.querySelector('#trending');
     return document.querySelector('#trending-product-template').content.cloneNode(true);
 }
 
