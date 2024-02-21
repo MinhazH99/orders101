242fb7edf59830d5dfce59b2af7055f28269e633
MinhazH99 <minhazh@hotmail.co.uk>
2024-02-21 16:19:47 +0000

    basket can increase and decrease quantity

--- frontend/javascript/basket.js
+++ frontend/javascript/basket.js
@@ -1,14 +1,48 @@
 function removeCartItem() {
-    let cardRemove_btns = document.querySelectorAll('.cart-remove');
-    console.log(cardRemove_btns);
+    let cartRemove_btns = document.querySelectorAll('.cart-remove');
 
-    cardRemove_btns.forEach((btn) => {
+    cartRemove_btns.forEach((btn) => {
         btn.addEventListener('click', function () {
             btn.parentElement.remove();
+            updateTotal();
         });
     });
 }
 
 removeCartItem();
 
-export { removeCartItem };
+function updateTotal() {
+    let cartBoxes = document.querySelectorAll('.cart-box');
+    let totalPrice = document.querySelector('.total__price');
+    let total = 0;
+
+    cartBoxes.forEach((cartBox) => {
+        let priceElement = cartBox.querySelector('cart-box__product_price');
+        let price = parseFloat(priceElement.innerHTML.replace('£', ''));
+        let quantity = cartBox.querySelector('.cart-box__product-quantity').nodeValue;
+        total += price * quantity;
+    });
+
+    total = total.toFixed(2);
+
+    totalPrice.innerHTML = '£' + total;
+}
+
+function increaseInQuantity() {
+    let cartBoxes = document.querySelectorAll('.cart-box');
+    cartBoxes.forEach((cartBox) => {
+        let currentQuantityElement = document.querySelector('.cart-box__product-quantity');
+        let currentQuantity = Number(
+            document.querySelector('.cart-box__product-quantity').innerHTML
+        );
+        let increaseInQuantitybtn = document.querySelector('.cart-box_quantity-increase');
+        increaseInQuantitybtn.addEventListener('click', function () {
+            currentQuantity += 1;
+            currentQuantityElement.innerHTML = currentQuantity;
+        });
+    });
+}
+
+increaseInQuantity();
+
+export { removeCartItem, updateTotal, increaseInQuantity };
--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -1,3 +1,3 @@
 import { appendProducts } from './products.js';
 import { attachEventListeners } from './home.js';
-import { removeCartItem } from './basket.js';
+import { removeCartItem, updateTotal, increaseInQuantity } from './basket.js';
