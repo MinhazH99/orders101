f3d43d7336142795a0aaba9a7f9ba6f64bd65930
MinhazH99 <minhazh@hotmail.co.uk>
2024-03-04 11:35:51 +0000

    simplied isUpdatedQuantityOne function

--- frontend/javascript/basket.js
+++ frontend/javascript/basket.js
@@ -76,7 +76,7 @@ function initiateQuantityButtons() {
 // TODO save quantity in session storage and make the decrease button faded out in index HTLM and when quantity increases turn it normal
 
 function isUpdatedQuantityOne(quantity) {
-    return quantity <= 1 ? 1 : false;
+    return quantity <= 1;
 }
 
 function decrementQuantity(quantity) {
