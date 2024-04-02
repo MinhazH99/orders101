332b84a1194e6eca9c90ebd8a9cbede404892e55
MinhazH99 <minhazh@hotmail.co.uk>
2024-04-02 18:28:41 +0100

    created categories section of page

--- react/orders101/src/pages/Home.jsx
+++ react/orders101/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Categories from "../components/Categories";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
@@ -6,6 +7,7 @@ function Home() {
     <div>
       <Header />
       <Navbar />
+      <Categories />
     </div>
   );
 }
