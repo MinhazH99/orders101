9b7cbe944c210aeef0c4dfccc9547dadeecd5695
MinhazH99 <minhazh@hotmail.co.uk>
2024-04-02 18:19:00 +0100

    created navbar

--- react/orders101/src/main.jsx
+++ react/orders101/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "/src/css/orders101-ui.css";
-import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
+    <Home />
   </React.StrictMode>
 );
---
+++ react/orders101/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
