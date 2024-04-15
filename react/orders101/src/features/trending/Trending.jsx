fed36078542586f3c62bc3db107d02f869e2a1d6
MinhazH99 <minhazh@hotmail.co.uk>
2024-04-15 11:58:54 +0100

    update template section using components and props

--- react/orders101/src/features/trending/Trending.jsx
+++ react/orders101/src/features/trending/Trending.jsx
@@ -1,24 +1,23 @@
+import TrendingImage from "./TrendingImage";
+import TrendingProductDetails from "./TrendingProductDetails";
+
 function Trending() {
   return (
-    <div id="trending" class="trending grid">
-      <template id="trending-product-template">
+    <div>
+      <h2 class="subtitle">Trending now</h2>
+      <div id="trending" class="trending grid">
         <div class="trending-div-product-grid">
           <a href="#popular">
             <div class="trending-div-product-image">
-              <img
-                id="trending-div-product-image"
-                class="site-image"
-                src=""
-              ></img>
+              <TrendingImage />
             </div>
-            <div id="trending-div-product-label">Placeholder</div>
-            <div id="trending-div-product-price">£XX.XX</div>
+            <TrendingProductDetails />
           </a>
           <button class="trending-div-product-addcart btn-buy">
             Add to Basket
           </button>
         </div>
-      </template>
+      </div>
     </div>
   );
 }
---
+++ react/orders101/src/features/trending/TrendingImage.jsx
@@ -0,0 +1,7 @@
+function TrendingImage({ src = "../src/assets/trending-product.webp" }) {
+  return (
+    <img id="trending-div-product-image" class="site-image" src={src}></img>
+  );
+}
+
+export default TrendingImage;
