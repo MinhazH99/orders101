1e3e811b63936ef30238a94234f4e55732369452
MinhazH99 <minhazh@hotmail.co.uk>
2024-04-16 17:04:36 +0100

    formatting change to features

--- react/orders101/src/features/usp/Features.jsx
+++ react/orders101/src/features/usp/Features.jsx
@@ -3,14 +3,14 @@ function Features() {
     <section class="subsection">
       <div class="features container">
         <div>
-          <img src="../src/assets//bag-check.svg" alt=""></img>Free click &
+          <img src="../src/assets/bag-check.svg" alt=""></img>Free click &
           collect
         </div>
         <div>
-          <img src="../src/assets//cart.svg" alt=""></img> 90 day return policy
+          <img src="../src/assets/cart.svg" alt=""></img> 90 day return policy
         </div>
         <div>
-          <img src="../src/assets//truck.svg" alt=""></img> Free delivery on
+          <img src="../src/assets/truck.svg" alt=""></img> Free delivery on
           1000s of products
         </div>
       </div>
