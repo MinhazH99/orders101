c4da5eaf5e8173f8fb93d2805b4855487087042b
MinhazH99 <minhazh@hotmail.co.uk>
2024-04-16 11:26:59 +0100

    updated banner attribute

--- react/orders101/src/features/banner/Banner.jsx
+++ react/orders101/src/features/banner/Banner.jsx
@@ -3,7 +3,7 @@ function Banner() {
     <div class="container">
       <img
         class="banner-image"
-        srcset="../src/assets/banner3-360w.jpg 360w, 
+        srcSet="../src/assets/banner3-360w.jpg 360w, 
         ../src/assets/banner2-1084w.jpg 1084w, 
         ../src/assets/banner-1400w.webp 1400w"
         src="../src/assets//banner3-360w.jpg"
