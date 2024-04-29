0a2c598c091503831a25c88d8e6b9e7cd8249ca5
MinhazH99 <minhazh@hotmail.co.uk>
2024-04-29 12:09:39 +0100

    wrapped everything in React Fragment

--- react/orders101/src/features/navbar/Navbar.jsx
+++ react/orders101/src/features/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import BasketOpen from "../BasketOpen";
 import Hamburger from "../Hamburger";
 import { ShoppingCartContext } from "../../App";
@@ -16,40 +16,42 @@ function Navbar() {
   };
 
   return (
-    <div class="nav container">
-      <div class="hamburger__btn-open">
-        <Hamburger />
-      </div>
-      <a class="nav__icons" href="#stores">
-        <div class="nav__icons__item container">
-          <img
-            class="searchicons"
-            src="../src/assets/geo-alt.svg"
-            alt="geo locator"
-          ></img>
-          <span>Stores</span>
+    <React.Fragment>
+      <div class="nav container">
+        <div class="hamburger__btn-open">
+          <Hamburger />
         </div>
-      </a>
-      <div class="logo">
-        <p>DIYToolWorld</p>
-      </div>
-      <a class="nav__icons" href="#ideas">
-        <div class="nav__icons__item container">
-          <img
-            class="searchicons"
-            src="../src/assets/lightbulb.svg"
-            alt="lightbulb"
-          ></img>
-          <span>Ideas & Advice</span>
+        <a class="nav__icons" href="#stores">
+          <div class="nav__icons__item container">
+            <img
+              class="searchicons"
+              src="../src/assets/geo-alt.svg"
+              alt="geo locator"
+            ></img>
+            <span>Stores</span>
+          </div>
+        </a>
+        <div class="logo">
+          <p>DIYToolWorld</p>
         </div>
-      </a>
+        <a class="nav__icons" href="#ideas">
+          <div class="nav__icons__item container">
+            <img
+              class="searchicons"
+              src="../src/assets/lightbulb.svg"
+              alt="lightbulb"
+            ></img>
+            <span>Ideas & Advice</span>
+          </div>
+        </a>
 
-      <BasketOpen openCart={openCart} />
-      <div class="nav__search-container">
-        <input class="search-bar" type="search" placeholder="Search..." />
+        <BasketOpen openCart={openCart} />
+        <div class="nav__search-container">
+          <input class="search-bar" type="search" placeholder="Search..." />
+        </div>
       </div>
       <Cart isOpen={isCartOpen} closeCart={closeCart} />
-    </div>
+    </React.Fragment>
   );
 }
 
