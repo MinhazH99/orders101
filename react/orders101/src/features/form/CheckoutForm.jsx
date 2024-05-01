3d2514e5e1e49b308c4e870a964df8573679e212
MinhazH99 <minhazh@hotmail.co.uk>
2024-05-01 12:58:14 +0100

    handle post and patch requests

--- react/orders101/src/features/form/CheckoutForm.jsx
+++ react/orders101/src/features/form/CheckoutForm.jsx
@@ -8,6 +8,183 @@ function CheckoutForm() {
   const deliveryStreet = useRef(null);
   const deliveryPostCode = useRef(null);
 
+  let storage = {};
+  let order = {};
+  let total = 0;
+
+  function handleClick() {
+    handlePatchRequest();
+    handlePostRequest();
+    console.log(JSON.stringify(order));
+  }
+
+  function buildStockAvailabilityUrl(productId, qty) {
+    return (
+      "http://localhost:8081/products/stock-availability/" +
+      productId +
+      "?inc=false" +
+      "&qty=" +
+      qty
+    );
+  }
+
+  function patchOrder(url) {
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function handlePatchRequest() {
+    Object.keys(sessionStorage).forEach((key) => {
+      storage[key] = JSON.parse(sessionStorage.getItem(key));
+      let productId = key;
+      let qty = storage[key].quantity;
+      let unitCost = storage[key].totalCost;
+      total += unitCost;
+
+      let apiUrl = buildStockAvailabilityUrl(productId, qty);
+
+      patchOrder(apiUrl);
+    });
+  }
+
+  function handlePostRequest() {
+    let firstName = customerFirstName.current.value;
+    let secondName = customerSecondName.current.value;
+    let emailAddress = customerEmailAddress.current.value;
+
+    let streetName = deliveryStreet.current.value;
+    let postCode = deliveryPostCode.current.value;
+    let country = deliveryCountry.current.value;
+
+    updateOrder(
+      firstName,
+      secondName,
+      emailAddress,
+      streetName,
+      postCode,
+      country
+    );
+
+    let postURL = "http://localhost:8080/orders/";
+    postOrder(postURL, order);
+  }
+
+  function postOrder(url, order) {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function updateOrder(
+    firstName,
+    secondName,
+    emailAddress,
+    streetName,
+    postCode,
+    country
+  ) {
+    updateOrderWithCustomerDetails(
+      order,
+      firstName,
+      secondName,
+      emailAddress,
+      streetName,
+      postCode,
+      country
+    );
+
+    updateOrderWithDeliveryAddress(order, streetName, postCode, country);
+
+    updateOrderWithCartItems(order, storage);
+
+    order.totalPrice = total;
+    order.paymentStatus = "AUTHORISED";
+    order.orderStatus = "COMPLETED";
+
+    updateOrderWithCurrentDate(order);
+  }
+
+  function formatDate(date) {
+    let mm = date.getMonth();
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    let dd = date.getDate();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    let yyyy = date.getFullYear();
+
+    return yyyy + "-" + mm + "-" + dd;
+  }
+
+  function updateOrderWithCustomerDetails(
+    order,
+    firstName,
+    secondName,
+    emailAddress,
+    streetName,
+    postCode,
+    country
+  ) {
+    order.customer = {
+      name: firstName + " " + secondName,
+      email: emailAddress,
+      invoiceAddress: {
+        addressLine1: streetName,
+        postCode: postCode,
+        country: country,
+      },
+    };
+  }
+
+  function updateOrderWithDeliveryAddress(
+    order,
+    streetName,
+    postCode,
+    country
+  ) {
+    order.deliveryAddress = {
+      addressLine1: streetName,
+      postCode: postCode,
+      country: country,
+    };
+  }
+
+  function updateOrderWithCartItems(order, storage) {
+    order.basket = { lineItems: [] };
+    Object.keys(sessionStorage).forEach((key) => {
+      storage[key] = JSON.parse(sessionStorage.getItem(key));
+      let name = storage[key].name;
+      let unitPrice = storage[key].unitPrice;
+      let quantity = storage[key].quantity;
+      order.basket.lineItems.push({
+        name: name,
+        description: "test",
+        unitPrice: unitPrice,
+        quantity: quantity,
+      });
+    });
+  }
+
+  function updateOrderWithCurrentDate(order) {
+    const today = new Date();
+
+    order.createdDate = formatDate(today);
+  }
+
   return (
     <div>
       <div class="checkout__child">
