5d2f8459aa07d7742aeb6dd92597a1592586e6cd
MinhazH99 <minhazh@hotmail.co.uk>
2024-05-02 13:23:52 +0100

    cleaned up imports

--- react/orders101/src/features/trending/AddBasket.jsx
+++ react/orders101/src/features/trending/AddBasket.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { ShoppingCartContext } from "../../App";
 
 function AddBasket({ id, name, unitPrice, quantity }) {
